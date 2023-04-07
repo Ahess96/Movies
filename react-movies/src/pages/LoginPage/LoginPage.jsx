8a3526176234249f7ffefc46d6f13fc2ad6f23cf
Ahess96 <austinhess1996@comcast.net>
2023-04-07 16:36:57 -0700

    Actually complete part 1

--- react-movies/src/pages/LoginPage/LoginPage.jsx
+++ react-movies/src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function LoginPage({setUser2}) {
+    const [userState, setUserState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        error: ''
+    })
+    function handleChange(evt) {
+        setUserState({...userState, [evt.target.name]: evt.target.value, error: ''})
+    }
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        setUser2(userState);
+    }
+    const disable = userState.password !== userState.confirm;
   return (
     <>
-      <h1>LoginPage</h1>
+      <div className='form'>
+        <form autoComplete='off' onSubmit={handleSubmit}>
+            <label>Name</label>
+                <input type="text" name='name' value={userState.name} onChange={handleChange} required />
+            <label>Email</label>
+                <input type="email" name='email' value={userState.email} onChange={handleChange} required />
+            <label>Password</label>
+                <input type="password" name="password" value={userState.password} onChange={handleChange} required />
+            <label>Confirm</label>
+                <input type="password" name='confirm' value={userState.confirm} onChange={handleChange} required />
+            <button type='submit' disabled={disable}>Sign In</button>
+        </form>
+      </div>
     </>
   )
 }
-
